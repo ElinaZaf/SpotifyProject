8cc1ae8237f07ab82517b5c9972778bb61e9cbba
AngelZaf8 <73531566+AngelZaf8@users.noreply.github.com>
2021-07-07 11:35:51 +0300

    - created MainController - created _MainLayoutPage - moved user to Home and deleted UserController and UserView - added Views to Main(Premium, Support(empty), FAQ, SignIn, SignUp

---
+++ Omadiko.WebApp/Controllers/MainController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Omadiko.WebApp.Controllers
+{
+    public class MainController : Controller
+    {
+        // GET: Main
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Premium()
+        {
+            return View();
+        }
+        public ActionResult Support()
+        {
+            return View();
+        }
+        public ActionResult Faq()
+        {
+            return View();
+        }
+
+        public ActionResult SignIn()
+        {
+            return View();
+        }
+
+        public ActionResult SignUp()
+        {
+            return View();
+        }
+    }
+}
