218a787d2511e59668da4cc5d4ee9a6725c5aa6a
AngelZaf8 <73531566+AngelZaf8@users.noreply.github.com>
2021-08-13 09:15:05 +0300

    - added _LoginPartial to _MainNavigationBar - updated Account Views: Login/Register/ForgotPassword/ForgotPasswordConfirmation

--- Omadiko.WebApp/App_Start/FilterConfig.cs
+++ Omadiko.WebApp/App_Start/FilterConfig.cs
@@ -8,6 +8,7 @@ namespace Omadiko.WebApp
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
             filters.Add(new HandleErrorAttribute());
+            filters.Add(new AuthorizeAttribute());
         }
     }
 }
--- Omadiko.WebApp/Controllers/MainController.cs
+++ Omadiko.WebApp/Controllers/MainController.cs
@@ -6,6 +6,7 @@ using System.Web.Mvc;
 
 namespace Omadiko.WebApp.Controllers
 {
+    [AllowAnonymous]
     public class MainController : Controller
     {
         // GET: Main
