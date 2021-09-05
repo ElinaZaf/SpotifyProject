c6db6b6f721831a9704b8cadc3fca6aa596a867b
AngelZaf8 <73531566+AngelZaf8@users.noreply.github.com>
2021-09-05 16:30:43 +0300

    - added heart (not yet complete)

---
+++ Omadiko.WebApp/Controllers/SubscriberController.cs
@@ -0,0 +1,46 @@
+﻿using Omadiko.Database;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+using System.Web.Http.Results;
+using Omadiko.Entities;
+
+namespace Omadiko.WebApp.Controllers
+{
+    public class SubscriberController : Controller
+    {
+
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+
+        // GET: Subscriber
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        public JsonResult addFavoriteAlbum(int albumId)
+        {
+            var user = db.Users.Find(User.Identity.GetUserId());
+            var album = db.Albums.Find(albumId);
+            bool result;
+            if (!(user.FavouriteAlbums.Any(x => x.AlbumId == albumId)))
+            {
+                user.FavouriteAlbums.Add(album);
+                db.SaveChanges();
+                result = true;
+            }
+            else
+            {
+                user.FavouriteAlbums.Remove(album);
+                db.SaveChanges();
+                result = false;
+            }
+            return Json(result, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
