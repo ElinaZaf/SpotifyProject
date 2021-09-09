46f92adc5cc294f9e28ebb9c1b3133d5c0d091ca
aidubxr <aidubxr@gmail.com>
2021-09-09 17:58:13 +0300

    update in Admin index view

--- Omadiko.WebApp/Controllers/AdminController.cs
+++ Omadiko.WebApp/Controllers/AdminController.cs
@@ -1,4 +1,6 @@
-﻿using Omadiko.Entities.Models;
+﻿using Omadiko.Database;
+using Omadiko.Entities;
+using Omadiko.Entities.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,9 +12,16 @@ namespace Omadiko.WebApp.Controllers
     [Authorize(Roles = Role.Admin)]
     public class AdminController : Controller
     {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
         // GET: Admin
         public ActionResult Index()
         {
+            ViewBag.TotalArtists = db.Artists.Count();
+            ViewBag.TotalUsers = db.Users.Count();
+            ViewBag.TotalAlbums = db.Albums.Count();
+            ViewBag.TotalSongs = db.Songs.Count();
+
             return View();
         }
         
